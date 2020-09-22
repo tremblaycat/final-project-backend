7637c713b72867196cdcb7f00e5be1017fa4eda7
tremblaycat <tremblay.cat@outlook.com>
2020-09-22 14:49:52 -0400

    heroku changes

--- connection.js
+++ connection.js
@@ -1,10 +1,17 @@
 const { Pool } = require("pg");
-const credentials = new Pool({
-  user: "postgres",
-  password: "password",
-  host: "localhost",
-  port: 5432,
-  database: "Final Project",
-  ssl: false,
+try {
+  // When not running via Heroku, this will load the .env file.
+  require("dotenv").config();
+} catch (e) {
+  // When running with Heroku, dotenv doesn't need to be available.
+}
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({
+  connectionString: connectionString,
+  ssl: connectionString.includes("localhost")
+    ? false
+    : {
+        rejectUnauthorized: false,
+      },
 });
-module.exports = credentials;
+module.exports = pool;
